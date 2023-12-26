f914ea17513f6f8aae4116312d8293138e40ad6f
Nenni Ramzi Wassim <wassimreal29@gmail.com>
2023-12-26 11:15:41 +0100

    v0.2 ui

--- GUI.py
+++ GUI.py
@@ -15,7 +15,7 @@ class MyGUI():
         app.columnconfigure((0,4), weight=1)
         app.columnconfigure((1,3), weight=3)
         app.columnconfigure(2, weight=6)
-        app.rowconfigure((1,3), weight=1)
+        app.rowconfigure(3, weight=1)
         app.rowconfigure((0,2), weight=2)
         
         #Title Page
@@ -28,25 +28,29 @@ class MyGUI():
         
         home_frame.grid_configure(row=0, column=2, sticky="we")
 
-        #label_insert=ctk.CTkLabel(app, text="Insert a CSV file", font=("Arial", 18)) #Text 
-        #label_insert.grid_configure(row=1, column=1, sticky='nw',  padx=20, pady=20)
-
         #textbox=tk.Text(app, height=3 ,font=("Arial", 16)) #Text Field
         #textbox.pack()
 
         #myentry= ctk.CTkEntry(app, width=350, height=40)
         #myentry.pack(pady=20)#TextField with 1 line
 
+        #---Import Frame---
+        file_frame=ctk.CTkFrame(app, height=50, corner_radius=10)
+
+        label_insert=ctk.CTkLabel(file_frame, text="Insert a CSV file", font=("Arial", 18)) #Text 
+        label_insert.grid_configure(row=1, column=1, sticky='ew',  padx=20, pady=20)
         #Import Button
         file_image=Image.open("icon.png")
         self.file_path = tk.StringVar()
-        btn_insert=ctk.CTkButton(app, text="Import", font=("Arial", 18), image=ctk.CTkImage(dark_image=file_image, size=(20,20)), compound="top", command=self.browse_file)
-        btn_insert.grid(row=2, column=1, sticky='w',  padx=20, )
+        btn_insert=ctk.CTkButton(file_frame, text="Import", font=("Arial", 18), image=ctk.CTkImage(dark_image=file_image, size=(20,20)), compound="top", command=self.browse_file)
+        btn_insert.grid_configure(row=2, column=1, sticky='we', padx=20, ipady=2)
         
         #Output File Label
-        self.label = ctk.CTkLabel(app, textvariable=self.file_path)
-        self.label.grid_configure(row=3, column=1)
+        self.label = ctk.CTkLabel(file_frame, textvariable=self.file_path)
+        self.label.grid_configure(row=3, column=1, sticky="we", padx=10, pady=10)
         
+        file_frame.grid_configure(row=2, column=0, )#sticky="ns")
+
 
         #Result Button
         btn_result=ctk.CTkButton(app, text="Result", font=("Arial", 18), command=self.show_path)
@@ -57,6 +61,11 @@ class MyGUI():
         self.algorithm=ctk.StringVar()
         list= ctk.CTkComboBox(app, values=["SVM","TensorFlow","KNN"], variable=self.algorithm, command=self.change_value)
         list.grid_configure(row=2, column=2)
+        
+        #Result Label
+        self.result = tk.StringVar()
+        self.result_label = ctk.CTkLabel(app, textvariable=self.result)
+        self.result_label.grid_configure(row=3, column=2)
         
         #buttonframe= tk.Frame(app)
         #buttonframe.columnconfigure(0, weight=1)
@@ -96,9 +105,9 @@ class MyGUI():
             print("CSV file")
 
         
-
     def show_path(self):
         # Display the selected file path
+        self.result.set("Accuracy = 90%")
         print("Selected File Path:", self.file_path.get())
 
 MyGUI()
